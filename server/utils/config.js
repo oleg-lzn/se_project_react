30bb8ca07060c149625fa534e11196beae0c11d8
Oleg <luzeninoleg2012@gmail.com>
2025-07-26 16:44:16 +0300

    added default values to config to make backend available

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,4 +1,8 @@
-const { JWT_SECRET, DB_URL, PORT = 3001 } = process.env;
+const {
+  JWT_SECRET = "CAT_FISH",
+  DB_URL = "mongodb://127.0.0.1:27017/wtwr_db",
+  PORT = 3001,
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
