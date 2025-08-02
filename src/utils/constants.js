7cc3c21149da9dc3517823c2ff0101ac5cc2d19c
Oleg <luzeninoleg2012@gmail.com>
2025-08-02 18:18:25 +0300

    login fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
 export const APIKey = "fb75fd6eba0cc218feadbef348aca578";
 export const latitude = 52.5244;
 export const longitude = 13.4105;
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr.euronatura.pt"
+    : "http://localhost:3001";
