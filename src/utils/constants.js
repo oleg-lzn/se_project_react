f4158f2db856b3a274582bcf64d6993c47436b09
Oleg <luzeninoleg2012@gmail.com>
2025-11-04 14:47:48 +0200

    change in env variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,4 @@
 export const APIKey = "fb75fd6eba0cc218feadbef348aca578";
 export const latitude = 52.5244;
 export const longitude = 13.4105;
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL
-    : "http://localhost:3001";
+export const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:3001";
